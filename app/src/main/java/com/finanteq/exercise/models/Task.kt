e977cf81d833e68d3a4980d4e348ba1ee0266271
Piotr Wasilewski <61659817+S17918@users.noreply.github.com>
2021-07-31 10:38:53 +0200

    Init commit

---
+++ app/src/main/java/com/finanteq/exercise/models/Task.kt
@@ -0,0 +1,10 @@
+package com.finanteq.exercise.models
+
+import java.util.*
+
+data class Task(
+    val task_name: String? = null,
+    val task_date: Date? = null,
+    val task_category: String? = null
+) {
+}
---
+++ app/src/main/java/com/finanteq/exercise/ui/SingleActivity.kt
@@ -0,0 +1,12 @@
+package com.finanteq.exercise.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.finanteq.exercise.R
+
+class SingleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
